347277c59ea5cdc3347efbc143d4095b9d199625
pletain <pletain@dgu.ac.kr>
2021-10-04 17:39:14 +0900

    회원 서비스 개발

---
+++ src/main/java/pletain/demo/service/MemberService.java
@@ -0,0 +1,35 @@
+package pletain.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import pletain.demo.domain.Member;
+import pletain.demo.repository.MemberRepository;
+import pletain.demo.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
