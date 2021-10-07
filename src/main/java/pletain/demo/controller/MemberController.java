4c58caa1cb85a2c8cae29625877f268c5aa31880
pletain <pletain@dgu.ac.kr>
2021-10-07 23:57:00 +0900

    회원 웹 기능 웹 MVC 개발

--- src/main/java/pletain/demo/controller/MemberController.java
+++ src/main/java/pletain/demo/controller/MemberController.java
@@ -2,6 +2,7 @@ package pletain.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import pletain.demo.service.MemberService;
 
@@ -13,5 +14,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
