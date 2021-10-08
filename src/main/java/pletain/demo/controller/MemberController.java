6554dfb13cbff8e4e0c37da16fb1d247e08fa7de
pletain <pletain@dgu.ac.kr>
2021-10-08 20:23:06 +0900

    회원 등록

--- src/main/java/pletain/demo/controller/MemberController.java
+++ src/main/java/pletain/demo/controller/MemberController.java
@@ -3,7 +3,9 @@ package pletain.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import pletain.demo.domain.Member;
 import pletain.demo.service.MemberService;
 
 @Controller
@@ -19,5 +21,15 @@ public class MemberController {
     @GetMapping(value = "/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
